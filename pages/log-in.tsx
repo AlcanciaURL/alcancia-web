b53d95388b3dc4398388320dd6460468a3dfabb2
Alevsdl <alejandravsdl@gmail.com>
2023-03-07 23:31:16 -0600

    Initial login design

--- pages/log-in.tsx
+++ pages/log-in.tsx
@@ -1,7 +1,7 @@
 import Navbarlogin from '@/components/navbarlogin/Navbarlogin'
 
 
-const SignIn = () => {
+const LogIn = () => {
 
     return (
         <div className="bg-[white] ">
@@ -9,19 +9,19 @@ const SignIn = () => {
             <div className="flex items-center justify-center h-screen ">
                 <div className="bg-[#F0F7F4] rounded-lg border shadow-sm p-10 pt-20 border-8 border-[#B3BFB8] sm:w-[40%] w-[100%]">
                     <div className="border-dashed border-2 border-[#329F5B] raunded-xl p-3 ">
-                        <label className="block mb-2 text-base font-medium text-[#3C493F] ">Nombre</label>
-                        <input className="bg-[#CCDBD2] appearance-none rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#CCDBD2] focus:border-2" type="text" />
+
                         <label className="block mb-2 text-base font-medium text-[#3C493F] ">Email</label>
                         <input className="bg-[#CCDBD2] appearance-none rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#CCDBD2] focus:border-2" type="text" />
                         <label className="block mb-2 text-base font-medium text-[#3C493F] ">Contraseña</label>
                         <input className="bg-[#CCDBD2] appearance-none rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#CCDBD2] focus:border-2" type="text" />
-                        <label className="block mb-2 text-base font-medium text-[#3C493F] ">Confirmar contraseña</label>
-                        <input className="bg-[#CCDBD2] appearance-none rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#CCDBD2] focus:border-2" type="text" />
                     </div>
-                    <div className="flex items-center justify-center pt-5">
+                    <div className="flex items-center justify-between pt-5">
                         <button className="bg-[#329F5B] hover:bg-[#CCDBD2] text-white font-bold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline" type="button">
-                            Registrarme
+                            Iniciar Sesión
                         </button>
+                        <a className="inline-block align-baseline font-semibold text-sm text-[#329F5B] hover:text-[#B3BFB8]" href="#">
+                            ¿Olvisate tu contraseña?
+                        </a>
                     </div>
                 </div>
             </div>
@@ -30,4 +30,4 @@ const SignIn = () => {
 }
 
 
-export default SignIn
+export default LogIn
