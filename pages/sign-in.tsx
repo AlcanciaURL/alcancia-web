eb0216013446bcc29b9085396762b8a445208518
Alevsdl <alejandravsdl@gmail.com>
2023-03-07 22:29:52 -0600

    New branch

---
+++ pages/sign-in.tsx
@@ -0,0 +1,20 @@
+import Navbarlogin from '@/components/navbarlogin/Navbarlogin'
+import { ReactNode, useEffect } from 'react'
+
+
+const SignIn = () => {
+  
+  return (
+    <div className="bg-[white] ">
+      <Navbarlogin ></Navbarlogin>
+      <div className="flex items-center justify-center h-screen ">
+        <div className="bg-[#F0F7F4] rounded-lg border shadow-sm p-10 border-2 border-[#B3BFB8] w-[40%] ">
+            <div className="bg-[red]">jeje</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default SignIn
